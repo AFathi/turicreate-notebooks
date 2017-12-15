80eeffc9f6c3d21e3cec65a441b99a7bc884ef23
AFathi <me@ahmedbekhit.com>
2017-12-14 19:22:33 -0500

    updated first notebook

--- raw_python/sample_reading.py
+++ raw_python/sample_reading.py
@@ -1,6 +1,6 @@
 # Import Turi Create package
 import turicreate as turi
 # Reads the data file, in this case we will read data from a CSV file
-dataBuffer = turi.SFrame.read_csv('sampleFile.csv')
+dataBuffer = turi.SFrame.read_csv('src/notebooks/data/SampleCSVFile_119kb.csv')
 # Parses out the first 5 rows (counting begins from 0 to 4)
 dataBuffer.head(4)
